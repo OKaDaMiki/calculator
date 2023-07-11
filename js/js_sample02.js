17262e09d02041f796d1ebb0e767c1b45485dcbb
OkdMk <138852873+OkdMk@users.noreply.github.com>
2023-07-11 21:58:00 +0900

    改良：先生に教えてもらいながらどうにか形になった

--- js/js_sample02.js
+++ js/js_sample02.js
@@ -18,17 +18,75 @@ console.log(numA % numB);
 const num1 = document.getElementById("num1");
 const num2 = document.getElementById("num2");
 const ans = document.getElementById("ans");
-const btn = document.getElementById("btn");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+let numC = "error";
+
 console.log(num1.value);
 console.log(num2.value);
 console.log(ans);
-console.log(btn);
+console.log(btn5);
 
-btn.addEventListener("click",() => {
-    console.log(Number(num1.value));
-    console.log(Number(num2.value));
-    ans.value = Number(num1.value) + Number(num2.value)  
+if(true){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+btn5.addEventListener("click",() => {
+    //console.log(numC);
+    // console.log(Number(num1.value));
+    // console.log(Number(num2.value));
+    if(numC == "+"){
+        console.log("足し算する");
+        ans.value = Number(num1.value) + Number(num2.value);
+    }
+    else if(numC == "-"){
+        ans.value = Number(num1.value) - Number(num2.value);
+    }
+    else if(numC == "*"){
+        ans.value = Number(num1.value) * Number(num2.value);
+    }
+    else if(numC == "/"){
+        ans.value = Number(num1.value) / Number(num2.value);
+    }
+    else {
+        console.log("error");
+        num1.value = "";
+        num2.value = "";
+    }
+
+    // if(btn1){
+    //     ans.value = Number(num1.value) + Number(num2.value)
+    // }
+    // else if(btn2){
+    //     ans.value = Number(num1.value) - Number(num2.value)
+    // }
+    // else if(btn3){
+    //     ans.value = Number(num1.value) * Number(num2.value)
+    // }
+    // else if(btn4){
+    //     ans.value = Number(num1.value) / Number(num2.value)
+    // }
+      
 });
-
-// どこに　なにをする　を書く
-// ansのvalueにnum1のvalueを表示
+btn1.addEventListener("click",() =>{
+//    console.log("+");
+    numC = "+";
+})
+btn2.addEventListener("click",() =>{
+    //    console.log("+");
+    numC = "-";
+})
+btn3.addEventListener("click",() =>{
+    //    console.log("+");
+    numC = "*";
+})
+btn4.addEventListener("click",() =>{
+    //    console.log("+");
+    numC = "/";
+})
